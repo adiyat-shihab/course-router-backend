aa1dc54aded8fbca0b9cbdd3e9d4d95fa858f99a
adiyat-shihab <mohammadshihab104@gmail.com>
2023-10-30 01:22:48 +0600

    create get and update method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -33,14 +33,72 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("dataDB");
+    const users = database.collection("users");
+
+    app.get("/user", async (req, res) => {
+      const cursor = users.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/user/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { email: id };
+        const result = await users.findOne(query);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    app.post("/addUser", async (req, res) => {
+      try {
+        const user = req.body;
+        console.log(user);
+        const result = await users.insertOne(user);
+        console.log(
+          `A document was inserted with the _id: ${result.insertedId}`
+        );
+        res.send(result);
+      } catch (error) {
+        res.send(error);
+      }
+    });
+
+    app.put("/course/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const data = req.body;
+        const userObject = {
+          courseName: data.name,
+          imageUrl: data.image,
+        };
+        const updateDoc = {
+          $push: {
+            user: [userObject],
+          },
+        };
+
+        const result = await users.updateOne(filter, updateDoc, options);
+        console.log(data);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
